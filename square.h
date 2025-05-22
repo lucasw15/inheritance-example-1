d279c2b994c75814276aeed4b9f95c149d8d7bf1
lucasw15 <witthauerl@spu.edu>
2025-05-22 12:38:42 -0700

    Update square.h

--- square.h
+++ square.h
@@ -14,6 +14,19 @@ public:
 	~Square();
 	string ToString()const;
 };
+// 1. What happened when you removed the Rectangle in the ToString()?
+// the square's ToString() no longer included the rectangle’s properties.
+// because Square inherits from Rectangle, and the base class's ToString() output is not included.
+
+
+// 2. Could you print the _width only in the square? Why?
+// Yes, I could access and print _width only if it was declared as protected since derived classes cannot access private from the original.
+
+
+
+// 3. What happened when you removed the Rectangle constructor call? Why?
+// the compiler gives an error or default-initialized class.
+// Rectangle doesn’t have a default constructor, so we must explicitly call the constructor in the Square list.
 
 
 #endif
